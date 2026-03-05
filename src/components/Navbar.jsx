c3b69ed33c7094f12c9e519a63d448b4af716e48
Srilakshman K <srilogan2612004@gmail.com>
2026-03-05 13:15:03 +0530

    Setup React project structure with Vite

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>Akaa Associate</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Akaa Associate</h1>
+    </>
+  );
+}
+
+export default Home;
